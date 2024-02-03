eac72af3e1411d978cb213dd601938ca8512ad54
fethi <fethisert0@gmail.com>
2024-02-03 17:38:49 +0200

    deepCopy function deepCopying methods and shallow copying methods

--- src/index.js
+++ src/index.js
@@ -1,147 +1,139 @@
 import { compose, pipe } from 'lodash/fp'
-
-const input  = " JavaScriprt ";
-const result = `<div>${input.trim()}</div>`
-// console.log(output);
+import { Map } from 'immutable';
 
 
-// const trim = text => text.trim();
-// const toLowerCase = text => text.toLowerCase();
-// const wrap = (input) => `<div>${input}</div>`
-//`<${type}>${input}</${type}>`
+// const mutant = { name: 'Wolverine'};
+// mutant.isPowerFull = true; //Add the prortie if it not exist
+// console.log(mutant);
 
-// const functionalResult = divWrap(toLowerCase(trim((input))));
+let book = Map({ title: "Harry Potter" });
 
-// console.log(res);
-//pipe and compse expect func ...
-// const composeTransform = compose(trim, toLowerCase, wrap('span')); // ==> divWrap(toLowerCase(trim((input))));
-                                                        //this function will give error compose(func) expect func
-// const composeRestult =  composeTransform(input);
-// console.log(composeRestult);
+// console.log(book); //Hard to use wtih other libraris beacuse it not like js object now
+// console.log(book.title); //returns undefined
+// console.log(book.get('title')); // I have to get method to ge
+// console.log(book.toJS());
 
-// const pipeTransform = pipe(trim, toLowerCase, wrap); // ==> divWrap(toLowerCase(trim((input))));
-// const pipeRestult =  pipeTransform(input);
-// console.log(pipeRestult);
+const publish = (book) => {
+    return book.set('isPublished',true); // it's not goint to return new object to mutate to orginal object
+}
+
+book = publish(book);
+
+console.log(book.toJS());
+
+//mutable and Immutable
+
+//mutable means change existet data
+
+//Primitives are Immutable
+let str = "Hello";
+// str[0] = 'C'; ==> error: Cannot assign to read only property '0' of string 'Hello
+// console.log(str);
+
+str =  'Cello' // it's not mutate the mutable variable it's reasign 
+// Reassingment and Mutable is not he same thing
+
+//Structures contain mutable data Yapilar arr[] object{} 
+
+const albhabet = ['a', 'b' , 'c' ,'d'];
+
+albhabet[0] = 'x'; //This is mutation of mutable data
+
+// console.log(albhabet);
 
 
-//Currying
 
-function add(a){
-    return function(b){
-        return a + b;
+const addToScoreHistory = (array, score) => { //Pure function don't mutate the given data
+                                              //React want pure function beacuse If add item like this setState not gonna understand if state change
+    return [...array,score];
+    // return array.push(score);
+}
+
+// const scoreArray = [10, 20, 40, 50];
+// const newScoreArray = addToScoreHistory(scoreArray, 500);
+// console.log(scoreArray);
+// console.log(newScoreArray);
+//Orginal array was mutate
+//const don't makes array immutables
+
+//Creating Shallow Copies
+
+// const numbers = [1, 2, 3, ,4 ,5];
+
+// // ...spread opeator
+// const spreadCopy = [numbers];
+// console.log(spreadCopy === numbers); //false
+
+// // Object.assign()
+// const objectAsgArr = Object.assign([],numbers);
+
+// console.log(objectAsgArr === numbers); //false
+
+
+//Note //mixxxx my brain dude
+
+// const arr = [1, 2, 3, 4];
+// const arrX = [...arr,[5, 6]];
+
+// const arrY = [...arrX];
+
+// arrY[4][0] = 500;
+// console.log(arrX);
+// console.log(arrY);
+
+//nested structual data still share reference
+
+//Note that Array.from() and Array.slice() also craete shallow copies of arr
+
+
+//Object.free
+const scoreObj = {
+    'first' : 44,
+    'second': 12,
+    'third': {'a': 1, 'b': 3}
+}
+
+// Object on which to lock the attributes.
+// Prevents the modification of existing property attributes and values, and prevents the addition of new properties.
+Object.freeze(scoreObj);
+// scoreObj.first = 23; // gives a error
+scoreObj.third.a = 31; //but here I can still change to value //It can be good for whic object has not reference type value
+
+//With deep copy we can avoid this;
+
+// Vanila JS way deep copy
+
+const vJSdeepCopy = JSON.parse(JSON.stringify(scoreObj)); //even reference value not change orginal element beacuse it's deep copy
+vJSdeepCopy.third.a = 5000;
+
+
+console.log(scoreObj);
+console.log(vJSdeepCopy);
+
+
+//Vanilla JavaScirpt DeepCopy function
+
+const deepCopy = (obj) => {
+    if((typeof obj !== 'object') || obj === null) return obj;
+
+    //create array or object
+    const newObject  = Array.isArray(obj) ? [] : {};
+
+    for(let key in obj){
+        const value = obj[key];
+
+        newObject[key] = deepCopy(value);
     }
-};
 
-// console.log(add); //will return function reference
-// console.log(add()); // will call to add function and return function reference that inside the add function
-// console.log(add()()); //will call to add function than return the add function than call the referenc fuction that return from add fumction
-
-const addArrow = (a) => (b) => a + b;   //it's currying
-
-console.log(addArrow(2)(4))
-
-//Adding Currying Method to our wrap function make it more functional
-
-//This is what I try to do *** down there
-
-// const trim = (text) => text.trim();
-// const toLowerCase = (text) => text.toLowerCase();
-// const wrap = (input) => (type) => `<${type}>${input}</${type}>`;
-
-// const curryPipeTransform = pipe(trim, toLowerCase, wrap);
-
-// const curryResult = pipeTransform(input)('div')
-// console.log(curryResult);
-
-//You can think ass
-// function wrap(input){
-//     return function(type) {
-//         return `<${type}>${input}</${type}>`;
-//     }
-// }
-
-
-//How to properly do it
-// const trim = (text) => text.trim();
-// const toLowerCase = (text) => text.toLowerCase();
-// const wrap = (type) => (input) => `<${type}>${input}</${type}>`;
-
-// console.log(wrap);
-
-// const pipeTransformWrap = pipe(trim, toLowerCase, wrap('div')); //pipe calls to reference // after wrap return a function i call them to
-
-const trim = (text) => text.trim();
-const toLowerCase = (text) => text.toLowerCase();
-const wrap = (type) => (input) => `<${type}>${input}</${type}>`;
-
-
-
-
-const pipeTransformWrap = pipe(trim, toLowerCase, wrap('div')); //pipe calls to reference // after wrap return a function i call them to
-
-const x = (type,text) => {
-    const pipeTransformWrap = pipe(trim, toLowerCase, wrap(type));
-    return pipeTransformWrap(text);
+    return newObject;
 }
 
-console.log(x('div', 'text'));
+const obj = {name: 'Jaykip', addres:{country: 'USA', city:'Mexico'}};
 
-const person = {
-    name:'Ahmet',
-    address: {
-        country:"KKTC", 
-        city:"Lefkosa"}, 
-    age:25
-}
+const deepCopyObj = deepCopy(obj);
 
-const newPerson = {...person,name:'Hasan'}; //...spread operator yaymak //overwrite name //! address still shallow copy !
-                                            //newPerson object it's deepcopy but address is shallowCopy 
-newPerson.address.city = 'Girne'; //address is shallow copy there
-newPerson.age = 45; //This not effect to orginal person object beacuse it's not hold a reference
-// console.log(person);
-// console.log(newPerson);
-
-const newPerson_1 = {...person, //spread the person object now I can overwrite
-                        name:"Jack",
-                        address: { //deepCopying creating new objcet
-                            ...person.address,
-                               city: 'Mexico'     
-                        }
-                    }
-// console.log(newPerson_1);
-//Shallow Copy stores the references of objects to the original memory address. 
-//Deep copy stores copies of the object's value.
-
-
-const numbers = [1, 2, 3, 5, 6];
-const addValue = 4;
-const index = numbers.indexOf(1);
-console.log(index);
-
-const added = [
-    ...numbers.slice(0, index + 1),
-    addValue,
-    ...numbers.slice(index + 1, numbers.length)
-];
-
-console.log(added);
-
-// const str = 'Hello';
-// console.log(str.slice(1,3));  //el  using slice with String and Array is different
-
-// const xc = [...str].slice(1,3); //[e,l];
-// console.log(xc);
-
-//Remove Item
-const removeItem  = 2;
-const removed = numbers.filter(n => n !== 2 );
-console.log(removed);
-
-//Update Item
-const updateItem = 3;
-const updated = numbers.map(n => n === updateItem ? 100 : n);
-console.log(updated);
-
+console.log( obj === deepCopyObj ); // === false
+console.log(deepCopyObj);
 
 
 
